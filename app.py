d17b5189b100f85c4bff1df5efeb37f13ebfbe2e
Vishal <76504332+Vish501@users.noreply.github.com>
2025-03-21 10:11:52 +0000

    getting info from user

--- app.py
+++ app.py
@@ -23,5 +23,15 @@ def main():
     # Setting title of the page
     st.head("Blog Generator 9000 📓")
 
+    # Getting topic, len of blog, and blog style from user
+    topic = st.text_input("Topic")
+    col1, col2 = st.columns([5,5])
+    with col1: lenght = st.text_input("Desired Length of the Blog")
+    with col2: style = st.selectbox("Blog Style", ("Casual Reader", "Engineers", "Researchers"))
+
+    submit = st.button("Generate Blog")
+
+    
+
 if __name__ == "__main__":
     main()
